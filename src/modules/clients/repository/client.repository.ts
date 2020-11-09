b8330f9db8f04e7f629abc6e10805d2ac569e6ea
Carlos Rojas <carojaspaz@gmail.com>
2020-11-09 18:28:43 -0500

    Implementacion

--- src/modules/clients/repository/client.repository.ts
+++ src/modules/clients/repository/client.repository.ts
@@ -6,6 +6,8 @@ type Response = Either<
 
 export interface IClientRepo {
     updateClientPic(id: string, url: string) : Promise<Response>
+    registerClient(clientDto: any): Promise<Response>
+    getClientByEmail(email: string): Promise<Response>
 }
 
 export class ClientRepository implements IClientRepo {
@@ -15,8 +17,24 @@ export class ClientRepository implements IClientRepo {
     constructor(models: any){
         this.models = models
     }
+    getClientByEmail(email: string): Promise<Either<Result<any>, Result<void>>> {
+        //TODO: 1 Devolver la información del cliente con su lista de cuentas asociadas
+        throw new Error("Method not implemented.");
+    }
+    public async registerClient(clientDto: any): Promise<Response> {
+        //TODO: 1. Registrar cliente en la base de datos
+        //TODO: 2. Crear usuario
+        //         Validar que se cree el usuario
+        //TODO: 3. Actualización del cliente
+        //         Asignar el usuario creado al cliente
+        throw new Error("Method not implemented.");
+    }
+
+    
 
     public async updateClientPic(id: string, url: string): Promise<Response> {
+        //TODO: 1. Cargar imagen en S3
+        //TODO: 2. Actualizar el campo urlPicture del cliente
         throw new Error("Method not implemented.");
     }
     
