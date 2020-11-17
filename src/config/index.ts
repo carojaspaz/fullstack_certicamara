82d8f88248ff14e909f3f55a72cd079c3e012d21
Carlos Rojas <carojaspaz@gmail.com>
2020-11-17 10:54:15 -0500

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,9 +9,9 @@ const port = process.env.PORT || 4000
 const urlDatabase = process.env.DATA_DB_URL || 'XXXX'
 const jwtSecret = process.env.JWT_SECRET || 'MDJPPt3O5CDfhueyi0uV'
 const awsBucket = process.env.AWS_BUCKET || "fullstack-demo"
-const awsId = process.env.AWS_ID || "AKIAYNQVSAE2TMJ4BF2X"
-const awsSecret = process.env.AWS_SECRET || "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"
-const awsRegion = process.env.AWS_REGION || 'us-east-2'
+const awsId = process.env.AWS_ID || ""
+const awsSecret = process.env.AWS_SECRET || ""
+const awsRegion = process.env.AWS_REGION || ""
 
 // Export configutation
 export const Config = {
